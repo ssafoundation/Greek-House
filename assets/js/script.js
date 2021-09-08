aff0c2116697e60bdedd7d7ff4b27457e3fba6da
Akhtaruzzaman <akhtr01947@gmail.com>
2021-09-09 04:36:36 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+(function ($) {
+  "use strict";
+  $(document).ready(function () {
+    $(".partner__slider").slick({
+      infinite: true,
+      slidesToShow: 7,
+      slidesToScroll: 1,
+      settings: "unslick",
+
+      autoplay: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
+    $(".design__slider").slick({
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      variableWidth: true,
+    });
+    $(".testimonial_wrap").owlCarousel({
+      loop: true,
+      items: 1,
+      nav: false,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      dots: true,
+    });
+  });
+})(jQuery);
