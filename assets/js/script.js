63b32a83026b06a4624f41bdf7a348cda6c01360
Akhtaruzzaman <akhtr01947@gmail.com>
2021-09-10 23:39:28 +0600

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,9 @@
       slidesToShow: 6,
       slidesToScroll: 1,
       settings: "unslick",
-
+      autoplay: true,
+      autoplaySpeed: 3000,
+      speed: 600,
       autoplay: true,
       responsive: [
         {
@@ -37,22 +39,93 @@
       slidesToShow: 4,
       slidesToScroll: 1,
       autoplay: true,
+      autoplaySpeed: 3000,
+      speed: 600,
       variableWidth: true,
     });
-    // $(".product__slider__items").slick({
-    //   infinite: true,
-    //   slidesToShow: 5,
-    //   slidesToScroll: 1,
-    //   autoplay: true,
-    //   variableWidth: true,
-    // });
+
+    $(".product__slider__items").slick({
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      speed: 600,
+      variableWidth: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
     $(".testimonial_wrap").owlCarousel({
       loop: true,
       items: 1,
       nav: false,
-      autoplay: true,
+      // autoplay: true,
       autoplayTimeout: 5000,
       dots: true,
+    });
+    // $(".mobile_view_lookBook_wrap").owlCarousel({
+    //   loop: true,
+    //   items: 1,
+    //   nav: false,
+    //   autoplay: true,
+    //   autoplayTimeout: 5000,
+    //   dots: true,
+    // });
+
+    $(".mobile_view_lookBook_wrap").slick({
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      centerMode: true,
+      variableWidth: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            centerPadding: "40px",
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            centerPadding: "40px",
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            centerPadding: "40px",
+          },
+        },
+      ],
     });
     $(window).scroll(function () {
       var scroll = $(window).scrollTop();
