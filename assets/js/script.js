1f8b064dd94a3200abad65e2f6d8982e16f8f6b8
Akhtaruzzaman <akhtr01947@gmail.com>
2021-09-13 11:45:37 +0600

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,9 +87,17 @@
           },
         },
         {
+          breakpoint: 2048,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            variableWidth: true,
+          },
+        },
+        {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 4,
+            slidesToShow: 3,
             slidesToScroll: 1,
             variableWidth: true,
           },
