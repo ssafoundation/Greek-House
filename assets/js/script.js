6dd0b57ccf6ab9738f93da13fc563cc5b730f61d
Akhtaruzzaman <akhtr01947@gmail.com>
2021-09-22 23:45:07 +0600

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,10 +104,12 @@
       ],
     });
 
-    $(".product__slider__items").slick({
+    // product slider
+    var slider = $(".product__slider__items");
+    slider.slick({
       slidesToShow: 4,
       slidesToScroll: 1,
-      infinite: false,
+      infinite: true,
       autoplay: true,
       autoplaySpeed: 3000,
       speed: 600,
@@ -121,17 +123,9 @@
           },
         },
         {
-          breakpoint: 2048,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            variableWidth: true,
-          },
-        },
-        {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 5,
+            slidesToShow: 4,
             slidesToScroll: 1,
             variableWidth: true,
           },
@@ -156,6 +150,21 @@
         },
       ],
     });
+
+    function jumpBack() {
+      setTimeout(function () {
+        slider.slick("slickGoTo", 0);
+      }, 3000);
+    }
+
+    slider.on("afterChange", function (event, slick, currentSlide, nextSlide) {
+      console.log(currentSlide);
+      if (currentSlide === 4) {
+        console.log("last slide");
+        jumpBack();
+      }
+    });
+
     $(".testimonial_wrap").owlCarousel({
       loop: true,
       items: 1,
@@ -175,7 +184,7 @@
         },
         1000: {
           items: 1,
-          loop: false,
+          margin: 10,
         },
       },
     });
